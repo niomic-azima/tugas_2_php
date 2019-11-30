29b2f45ae75fa245e471ff5c9dcff8f81c7b7d9a
Azima_niomic <hades2317@gmail.com>
2019-11-30 22:32:54 +0700

    Tugas 2 PHP
    Tugas Niomic

---
+++ tugas2_1.php
@@ -0,0 +1,13 @@
+<?php
+  $hari = 36;
+  $pekerja_1 = 12;
+  $pekerja_2 = 24;
+
+  $hasil = $hari / $pekerja_1;
+  $x = $hasil * $pekerja_2;
+
+  echo "$hari Hari= $pekerja_1 Pekerja<br>";
+  echo "X Hari = $pekerja_2 Pekerja<br>";
+  echo "Jawaban : <br>";
+  echo "X = $x";
+ ?>
---
+++ tugas2_2.php
@@ -0,0 +1,12 @@
+<?php
+  $jual = 80000;
+  $beli = 50000;
+
+  $keuntungan = $jual - $beli;
+  $persen = $beli / $jual;
+  $hasil = $persen * 100;
+  echo "jual = 80000 <br>
+  beli = 50000 <br>";
+  echo "Keuntungan = $keuntungan ($hasil %)";
+
+ ?>
